055a2bc85f356efe9cfffb40f51f6f1e6125b5d2
DANTADYY <daiyan189@163.com>
2019-10-30 13:28:48 +0800

    002

--- Day01-15/Test01-15/hello.py
+++ Day01-15/Test01-15/hello.py
@@ -1,1 +1,34 @@
-print('hello world')
+# print('hello world').
+
+'''
+Python
+'''
+
+"""
+使用变量保存数据并进行算术运算
+
+Version: 0.1
+Author: 戴琰
+"""
+
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+
+e = 123
+f = 123.456
+g = 1 + 3j 
+h = 'hello world'
+i = False
+print(type(e)),# <class 'int'> 
+print(type(f)),# <class 'float'>
+print(type(g)),# <class 'complex'>
+print(type(h)),# <class 'string'>
+print(type(i)),# <class 'bool'>
