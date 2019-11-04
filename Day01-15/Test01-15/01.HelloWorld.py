d9c7b20d59e209d4553291e95b63b702fe1e8761
DANTADYY <daiyan189@163.com>
2019-11-04 16:55:03 +0800

    2019110401

--- Day01-15/Test01-15/01.HelloWorld.py
+++ Day01-15/Test01-15/01.HelloWorld.py
@@ -11,15 +11,15 @@ Version: 0.1
 Author: 戴琰
 """
 
-# a = 321
-# b = 123
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a // b)
-# print(a % b)
-# print(a ** b)
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
 
 
 e = 123
@@ -33,12 +33,3 @@ print(type(g)),# <class 'complex'>
 print(type(h)),# <class 'string'>
 print(type(i)),# <class 'bool'>
 
-a = int(input('a = '))
-b = int(input('b = '))
-print('%d + %d = %d' % (a, b, a + b))
-print('%d - %d = %d' % (a, b, a - b))
-print('%d * %d = %d' % (a, b, a * b))
-print('%d / %d = %f' % (a, b, a / b))
-print('%d // %d = %d' % (a, b, a // b))
-print('%d %% %d = %d' % (a, b, a % b))
-print('%d ** %d = %d' % (a, b, a ** b))
