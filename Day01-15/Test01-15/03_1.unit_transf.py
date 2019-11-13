241e7b4f708fafce612ba92d0e3c904e5b5b801f
DANTADYY <daiyan189@163.com>
2019-11-13 17:43:07 +0800

    0003

--- Day01-15/Test01-15/03_1.unit_transf.py
+++ Day01-15/Test01-15/03_1.unit_transf.py
@@ -11,3 +11,27 @@ elif unit == 'cm' or unit == '厘米':
     print('%f厘米 = %f英寸' % (value,value / 2.54))
 else:
     print('请输入有效单位')
+
+#**要求**：如果输入的成绩在90分以上（含90分）输出A；80分-90分（不含90分）输出B；70分-80分（不含80分）输出C；60分-70分（不含70分）输出D；60分以下输出E。
+#参考答案：
+
+'''Python
+"""
+百分制成绩转换为等级制成绩
+
+Version: 0.1
+Author: daiyy
+"""
+'''
+score = float(input('请输入成绩：'))
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >=70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'E'
+print('对应的等级是:', grade)     
